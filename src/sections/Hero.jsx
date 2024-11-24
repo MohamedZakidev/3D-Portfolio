93e220850956ddf4c279a1a6f064e2fb695b12a6
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-11-25 01:41:23 +0200

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,5 @@
-import { PerspectiveCamera } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
 import { useMediaQuery } from "react-responsive"
 import Button from "../components/Button"
-import CanvasLoader from "../components/CanvasLoader"
-import HackerRoom from "../components/HackerRoom"
-import HeroCamera from "../components/HeroCamera"
-import ReactLogo from "../components/ReactLogo"
 import { calculateSizes } from "../constants"
 
 function Hero() {
@@ -28,42 +21,22 @@ function Hero() {
             </div>
 
             <div className="w-full h-full absolute inset-0">
-                {/* <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader />}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-                        <HeroCamera isMobile={isMobile} isDesktop={isDesktop}>
-                            <HackerRoom
-                                rotation={[0, -Math.PI, 0]}
-                                position={sizes.deskPosition}
-                                scale={sizes.deskScale}
-                            />
-                        </HeroCamera>
+                {/*  */}
+            </div>
 
-                        <group>
-                            {isDesktop && <ReactLogo position={sizes.reactLogoPosition} />}
-                            {/* <Target position={sizes.targetPosition} /> */}
-                {/* <Cube position={sizes.cubePosition} /> */}
-                {/* <Rings position={sizes.ringPosition} /> */}
-            </group>
-            <ambientLight intensity={1} />
-            <directionalLight position={[10, 10, 10]} intensity={0.5} />
-        </Suspense>
-                </Canvas > */
-}
-            </div >
-
-    <div className="c-space z-10 absolute bottom-7 left-0 right-0 w-full">
-        <a href="#about" className="w-fit">
-            <Button
-                containerClass="sm:w-fit w-full sm:min-w-96"
-                isBeam
-            >
-                Let’s work together
-            </Button>
-        </a>
-    </div>
-        </section >
+            <div className="c-space z-10 absolute bottom-7 left-0 right-0 w-full">
+                <a href="#about" className="w-fit">
+                    <Button
+                        containerClass="sm:w-fit w-full sm:min-w-96"
+                        isBeam
+                    >
+                        Let’s work together
+                    </Button>
+                </a>
+            </div>
+        </section>
     )
 }
+
 
 export default Hero
