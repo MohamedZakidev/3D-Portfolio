f48b98777974c6289de1ba467fba035b517efa84
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-11-25 01:10:45 +0200

    Update Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,6 +1,7 @@
 import { Center, OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Suspense, useState } from "react"
+import { useMediaQuery } from "react-responsive"
 import CanvasLoader from "../components/CanvasLoader"
 import DemoComputer from "../components/DemoComputer"
 import { myProjects } from "../constants"
@@ -10,6 +11,8 @@ function Projects() {
   const projectsCount = myProjects.length
   const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
   const currentProject = myProjects[selectedProjectIndex]
+  const isDesktop = useMediaQuery({ minWidth: 1024 })
+
 
   function handleNavigation(direction) {
     setSelectedProjectIndex(prevIndex => {
@@ -82,20 +85,22 @@ function Projects() {
         </div>
 
         {/* 2 */}
-        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
-          <Canvas>
-            <ambientLight intensity={Math.PI} />
-            <directionalLight position={[10, 10, 5]} />
-            <Center>
-              <Suspense fallback={<CanvasLoader />}>
-                <group scale={2} position={[-0.46, -3, 0]} rotation={[0, 0, 0]}>
-                  <DemoComputer texture={currentProject.texture} />
-                </group>
-              </Suspense>
-            </Center>
-            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
-          </Canvas>
-        </div>
+        {isDesktop && (
+          <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+            <Canvas>
+              <ambientLight intensity={Math.PI} />
+              <directionalLight position={[10, 10, 5]} />
+              <Center>
+                <Suspense fallback={<CanvasLoader />}>
+                  <group scale={2} position={[-0.46, -3, 0]} rotation={[0, 0, 0]}>
+                    <DemoComputer texture={currentProject.texture} />
+                  </group>
+                </Suspense>
+              </Center>
+              <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+            </Canvas>
+          </div>
+        )}
 
       </div>
     </section>
