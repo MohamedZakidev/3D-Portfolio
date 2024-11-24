f231686ee435a9bd1abec7b3a43318f900d99b2c
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-11-24 19:49:52 +0200

    add footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,25 @@
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <section className="c-space pt-7 border-t pb-3 border-black-300 flex justify-between items-center flex-wrap gap-5">
+            <div className="text-white-500 flex gap-2">
+                <p>Terms & Conditions</p>
+                <p> | </p>
+                <p>Privacy Policy</p>
+            </div>
+
+            <div className="flex gap-3">
+                <a
+                    href="https://github.com/MohamedZakidev"
+                    className="social-icon"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src="/assets/github.svg" alt="github logo"
+                        className="w-1/2 h-1/2"
+                    />
+                </a>
+            </div>
+        </section>
     )
 }
 
