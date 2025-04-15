e77e2cb9959c525ed71660b5992b6f52a712b3cc
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2025-04-15 13:23:43 +0200

    Update Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -31,13 +31,15 @@ function Projects() {
 
         {/* 1 */}
         <div className="rounded-2xl flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
-          <div className="absolute top-0 right-0">
-            <img
-              src={currentProject.spotlight}
-              alt="spotlight"
-              className="w-full h-96 object-cover rounded-2xl"
-            />
-          </div>
+          {currentProject.spotlight && (
+            <div className="absolute top-0 right-0">
+              <img
+                src={currentProject.spotlight}
+                alt="spotlight"
+                className="w-full h-96 object-cover rounded-2xl"
+              />
+            </div>
+          )}
           <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={currentProject.logoStyle}>
             <img
               src={currentProject.logo}
